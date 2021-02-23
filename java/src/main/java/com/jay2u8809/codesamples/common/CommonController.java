4e8b83f4c10299c3c9b9ebd2158dc6d0f79ab240
Jung Ian <jay2u8809@gmail.com>
2021-02-23 14:02:44 +0900

    Feat. Initial Settings

---
+++ java/src/main/java/com/jay2u8809/codesamples/common/CommonController.java
@@ -0,0 +1,9 @@
+package com.jay2u8809.codesamples.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommonController {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+}
