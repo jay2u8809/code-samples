4dea193005e3c4803ff3308572d9a4a422991fa3
Jung Ian <jay2u8809@gmail.com>
2021-02-28 23:34:17 +0900

    Fix. add update and delete functions

--- java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/web/PostsApiController.java
+++ java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/web/PostsApiController.java
@@ -30,4 +30,11 @@ public class PostsApiController extends CommonExtends {
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+
+        postsService.delete(id);
+        return id;
+    }
 }
