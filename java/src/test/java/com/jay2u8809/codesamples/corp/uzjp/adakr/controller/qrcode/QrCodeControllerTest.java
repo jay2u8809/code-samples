3c48a5762065e8ebcf6761d112a44ed600819e87
Jung Ian <jay2u8809@gmail.com>
2021-04-18 09:19:00 +0900

    Feat. QrCode Api test code

---
+++ java/src/test/java/com/jay2u8809/codesamples/corp/uzjp/adakr/controller/qrcode/QrCodeControllerTest.java
@@ -0,0 +1,44 @@
+package com.jay2u8809.codesamples.corp.uzjp.adakr.controller.qrcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class QrCodeControllerTest {
+/*   https://theheydaze.tistory.com/218   */
+/*
+“No tests found for given includes” when running Gradle tests in IntelliJ IDEA
+File > Setting (Ctrl+Alt+S)
+Build, Execution, Deployment > Build Tools > gradle
+Run Tests using: Intellij IDEA
+ */
+    private final String path = "/corp/uzjp/adakr/qrcode";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void moveGenerateQrCodeViewTest() throws Exception {
+
+        mockMvc.perform(get(path + "/entry/"))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void makeQrCodePath() throws Exception {
+
+        mockMvc.perform(post("/qrcode/generate/")
+                        .param("qrcodeUri", "www.naver.com"))
+                .andExpect(status().isOk());
+    }
+}
