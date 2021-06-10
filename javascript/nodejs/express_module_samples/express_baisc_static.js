3c0d5fbec4d4148983b11b278443181622f8f30f
J.ian <jay2u8809@gmail.com>
2021-06-10 22:18:41 +0900

    Feat. Add Express Module : Static module

---
+++ javascript/nodejs/express_module_samples/express_baisc_static.js
@@ -0,0 +1,20 @@
+
+import express from "express";
+
+// package.json : commonJS -> module
+import path from 'path';
+
+const __dirname = path.resolve();
+let app = express();
+
+// setting middleware
+app.use(express.static(__dirname + "/resources"));
+
+app.use((req, res) => {
+    res.writeHead(200, {    "Content-Type": "text/html" });
+    res.end("<img src='/testWallPaper.jpg' width='100%' />");
+});
+
+app.listen(52273, () => {
+    console.log(`Server Running Static Module`);
+});
