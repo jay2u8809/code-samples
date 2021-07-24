b90e9ce630bf17ef983853dcf13c1127fc361eae
Jung Ian <jay2u8809@gmail.com>
2021-07-25 01:42:33 +0900

    Fix. Change Member Table Info

---
+++ javascript/nodejs/nest-sample-project/src/common/code/MemberStatus.ts
@@ -0,0 +1,19 @@
+export enum MemberStatus {
+  /**
+   * 정상
+   * 通常
+   */
+  Normal,
+
+  /**
+   * 휴면
+   * 休眠
+   */
+  Dormancy,
+
+  /**
+   * 탈퇴
+   * 退会
+   */
+  Withdraw
+}
--- javascript/nodejs/nest-sample-project/src/entities/member/member.ts
+++ javascript/nodejs/nest-sample-project/src/entities/member/member.ts
@@ -1,17 +1,21 @@
 import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 import {BaseEntity} from "../base.entity";
+import {YN} from "../../common/code/YN";
+import {MemberStatus} from "../../common/code/MemberStatus";
+import {Gender} from "../../common/code/Gender";
 
 @Index("member_pkey", ["memberSn"], { unique: true })
 @Entity("member", { schema: "dev_schema" })
 export class Member extends BaseEntity {
+
   @PrimaryGeneratedColumn({ type: "bigint", name: "member_sn" })
-  memberSn: string;
+  memberSn: bigint;
 
   @Column("character varying", { name: "member_id", length: 300 })
   memberId: string;
 
-  @Column("character varying", { name: "member_password", length: 200 })
-  memberPassword: string;
+  @Column("character varying", { name: "member_pw", length: 200 })
+  memberPw: string;
 
   @Column("character varying", { name: "name_1", nullable: true, length: 100 })
   name_1: string | null;
@@ -53,87 +57,6 @@ export class Member extends BaseEntity {
   })
   phoneNo_2: string | null;
 
-  @Column("character varying", { name: "email_address", length: 200 })
-  emailAddress: string;
-
-  @Column("timestamp with time zone", { name: "member_signup_dt" })
-  memberSignupDt: Date;
-
-  @Column("character varying", {
-    name: "password_reset_request_yn",
-    length: 1,
-    default: () => "'N'",
-  })
-  passwordResetRequestYn: string;
-
-  @Column("character varying", {
-    name: "password_verif_media_code",
-    nullable: true,
-    length: 30,
-  })
-  passwordVerifMediaCode: string | null;
-
-  @Column("character varying", { name: "password_verif_key", length: 200 })
-  passwordVerifKey: string;
-
-  @Column("timestamp with time zone", {
-    name: "password_verif_send_dt",
-    nullable: true,
-  })
-  passwordVerifSendDt: Date | null;
-
-  @Column("timestamp with time zone", {
-    name: "password_verif_complete_exp_dt",
-    nullable: true,
-  })
-  passwordVerifCompleteExpDt: Date | null;
-
-  @Column("timestamp with time zone", {
-    name: "password_verif_complete_dt",
-    nullable: true,
-  })
-  passwordVerifCompleteDt: Date | null;
-
-  @Column("timestamp with time zone", {
-    name: "final_password_change_dt",
-    nullable: true,
-  })
-  finalPasswordChangeDt: Date | null;
-
-  @Column("character varying", {
-    name: "member_status_code",
-    length: 30,
-    default: () => "'Normal'",
-  })
-  memberStatusCode: string;
-
-  @Column("timestamp with time zone", {
-    name: "final_login_dt",
-    nullable: true,
-  })
-  finalLoginDt: Date | null;
-
-  @Column("numeric", {
-    name: "password_verif_failure_cnt",
-    precision: 10,
-    scale: 0,
-    default: () => "0",
-  })
-  passwordVerifFailureCnt: string;
-
-  @Column("character varying", {
-    name: "ac_lock_yn",
-    length: 1,
-    default: () => "'N'",
-  })
-  acLockYn: string;
-
-  @Column("timestamp with time zone", { name: "ac_lock_dt", nullable: true })
-  acLockDt: Date | null;
-
-  @Column("character varying", { name: "nickname", nullable: true, length: 50 })
-  nickname: string | null;
-
   @Column("character varying", {
     name: "country_code",
     nullable: true,
@@ -172,46 +95,134 @@ export class Member extends BaseEntity {
   })
   address_4: string | null;
 
-  @Column("character varying", { name: "dob_year", nullable: true, length: 4 })
-  dobYear: string | null;
+  @Column("character varying", { name: "email_address", length: 200 })
+  emailAddress: string;
 
-  @Column("character varying", { name: "dob_month", nullable: true, length: 2 })
-  dobMonth: string | null;
+  @Column("character varying", {
+    name: "birthday_year",
+    nullable: true,
+    length: 4,
+  })
+  birthdayYear: string | null;
 
-  @Column("character varying", { name: "dob_day", nullable: true, length: 2 })
-  dobDay: string | null;
+  @Column("character varying", {
+    name: "birthday_month",
+    nullable: true,
+    length: 2,
+  })
+  birthdayMonth: string | null;
+
+  @Column("character varying", {
+    name: "birthday_day",
+    nullable: true,
+    length: 2,
+  })
+  birthdayDay: string | null;
 
   @Column("character varying", {
     name: "gender_code",
     length: 30,
-    default: () => "'NoResponse'",
+    default: () => "'Nothing'",
   })
-  genderCode: string;
+  genderCode: Gender;
+
+  @Column("character varying", {
+    name: "member_status",
+    length: 30,
+    default: () => "'Normal'",
+  })
+  memberStatus: MemberStatus;
+
+  @Column("character varying", { name: "nickname", length: 50 })
+  nickname: string;
+
+  @Column("timestamp with time zone", {
+    name: "final_login_dt",
+    nullable: true,
+  })
+  finalLoginDt: Date | null;
+
+  @Column("timestamp with time zone", { name: "signup_dt" })
+  signupDt: Date;
+
+  @Column("character varying", {
+    name: "account_lock_yn",
+    length: 1,
+    default: () => "'N'",
+  })
+  accountLockYn: YN;
+
+  @Column("timestamp with time zone", {
+    name: "account_lock_dt",
+    nullable: true,
+  })
+  accountLockDt: Date | null;
+
+  @Column("timestamp with time zone", {
+    name: "account_rest_dt",
+    nullable: true,
+  })
+  accountRestDt: Date | null;
+
+  @Column("timestamp with time zone", {
+    name: "closed_account_dt",
+    nullable: true,
+  })
+  closedAccountDt: Date | null;
+
+  @Column("character varying", {
+    name: "closed_account_reason",
+    nullable: true,
+    length: 200,
+  })
+  closedAccountReason: string | null;
+
+  @Column("character varying", {
+    name: "pw_reset_request_yn",
+    length: 1,
+    default: () => "'N'",
+  })
+  pwResetRequestYn: YN;
+
+  @Column("character varying", { name: "pw_verify_key", length: 200 })
+  pwVerifyKey: string;
+
+  @Column("timestamp with time zone", {
+    name: "pw_verify_key_send_dt",
+    nullable: true,
+  })
+  pwVerifyKeySendDt: Date | null;
+
+  @Column("timestamp with time zone", {
+    name: "pw_verify_expect_dt",
+    nullable: true,
+  })
+  pwVerifyExpectDt: Date | null;
+
+  @Column("timestamp with time zone", {
+    name: "pw_verify_complete_dt",
+    nullable: true,
+  })
+  pwVerifyCompleteDt: Date | null;
+
+  @Column("timestamp with time zone", {
+    name: "final_pw_change_dt",
+    nullable: true,
+  })
+  finalPwChangeDt: Date | null;
+
+  @Column("numeric", {
+    name: "pw_verify_failure_cnt",
+    precision: 10,
+    scale: 0,
+    default: () => "0",
+  })
+  pwVerifyFailureCnt: string;
 
   @Column("character varying", {
     name: "mail_prohibit_yn",
     length: 1,
     default: () => "'N'",
   })
-  mailProhibitYn: string;
-
-  @Column("character varying", {
-    name: "admail_prohibit_yn",
-    length: 1,
-    default: () => "'N'",
-  })
-  admailProhibitYn: string;
-
-  @Column("character varying", {
-    name: "sms_prohibit_yn",
-    length: 1,
-    default: () => "'N'",
-  })
-  smsProhibitYn: string;
-
-  @Column("timestamp with time zone", {
-    name: "dm_prohibit_update_dm",
-    nullable: true,
-  })
-  dmProhibitUpdateDm: Date | null;
+  mailProhibitYn: YN;
 }
