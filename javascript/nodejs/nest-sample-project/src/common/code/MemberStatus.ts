bd60159aaa8f5b0dad92742101dba4df9dc653a6
Jung Ian <jay2u8809@gmail.com>
2021-07-25 15:08:26 +0900

    Fix. add Enum value

--- javascript/nodejs/nest-sample-project/src/common/code/MemberStatus.ts
+++ javascript/nodejs/nest-sample-project/src/common/code/MemberStatus.ts
@@ -3,17 +3,17 @@ export enum MemberStatus {
    * 정상
    * 通常
    */
-  Normal,
+  Normal = 'Normal',
 
   /**
    * 휴면
    * 休眠
    */
-  Dormancy,
+  Dormancy = 'Dormancy',
 
   /**
    * 탈퇴
    * 退会
    */
-  Withdraw
+  Withdraw = 'Withdraw'
 }
