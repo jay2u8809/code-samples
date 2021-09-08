b395b9993ed5b657856db75651c93e3af7a1b9c2
Jung Ian <jay2u8809@gmail.com>
2021-09-08 23:48:46 +0900

    Fix. SpringBoot GraphQL Query Resolver

--- java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/web/members/dto/MemberJoinResponseDto.java
+++ java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/web/members/dto/MemberJoinResponseDto.java
@@ -1,19 +1,52 @@
 package com.jay2u8809.codesamples.individual.study.bootandaws.web.members.dto;
 
+import com.jay2u8809.codesamples.common.dto.EmbeddedAddress;
+import com.jay2u8809.codesamples.common.dto.EmbeddedName;
+import com.jay2u8809.codesamples.common.dto.EmbeddedTel;
+import com.jay2u8809.codesamples.common.utils.EncryptUtils;
 import com.jay2u8809.codesamples.individual.study.bootandaws.domain.members.Member;
+import com.jay2u8809.codesamples.individual.study.bootandaws.domain.members.MemberAccountInfo;
 import lombok.Getter;
 import org.springframework.beans.BeanUtils;
+import org.springframework.util.ObjectUtils;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 public class MemberJoinResponseDto implements Serializable {
 
-    private Long memberSn;
-
-    private String memberId;
+    private final Long memberSn;
+    private final String memberId;
+    private final String memberPw;
+    private final String memberName1;
+    private final String memberName2;
+    private final String memberName3;
+    private final String memberName4;
+    private final String memberEmail;
+    private final String nickName;
+    private final String zipCode;
+    private final String address1;
+    private final String address2;
+    private final String address3;
+    private final String address4;
+    private final String phoneNo;
 
     public MemberJoinResponseDto(Member entity) {
-        BeanUtils.copyProperties(entity, this);
+        this.memberSn = entity.getMemberSn();
+        this.memberId = entity.getMemberId();
+        this.memberPw = "";
+        this.memberName1 = entity.getMemberName().getGivenName();
+        this.memberName2 = entity.getMemberName().getSurName();
+        this.memberName3 = entity.getMemberName().getGivenNameEn();
+        this.memberName4 = entity.getMemberName().getSurNameEn();
+        this.memberEmail = entity.getEmailAddress();
+        this.nickName = ObjectUtils.isEmpty(entity.getMemberAccountInfo()) ? null : entity.getMemberAccountInfo().getNickname();
+        this.zipCode = entity.getMemberAddress().getZipCode();
+        this.address1 = entity.getMemberAddress().getAddress1();
+        this.address2 = entity.getMemberAddress().getAddress2();
+        this.address3 = entity.getMemberAddress().getAddress3();
+        this.address4 = entity.getMemberAddress().getAddress4();
+        this.phoneNo = entity.getPhoneNo1().getPhoneNo();
     }
 }
--- java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/web/members/graphql/MemberQueryResolver.java
+++ java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/web/members/graphql/MemberQueryResolver.java
@@ -1,14 +1,22 @@
 package com.jay2u8809.codesamples.individual.study.bootandaws.web.members.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.jay2u8809.codesamples.common.CommonExtends;
 import com.jay2u8809.codesamples.individual.study.bootandaws.domain.members.Member;
 import com.jay2u8809.codesamples.individual.study.bootandaws.service.members.MemberService;
+import com.jay2u8809.codesamples.individual.study.bootandaws.web.members.dto.MemberJoinResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
-public class MemberQueryResolver implements GraphQLQueryResolver {
+@Transactional(readOnly = true)
+public class MemberQueryResolver extends CommonExtends implements GraphQLQueryResolver {
 
     private final MemberService memberService;
 
@@ -18,7 +26,30 @@ public class MemberQueryResolver implements GraphQLQueryResolver {
      * @param memberSn
      * @return
      */
-    public Member getMember(final Long memberSn) {
-        return this.memberService.findMemberBySn(memberSn);
+    public MemberJoinResponseDto getMember(final long memberSn) {
+        if (memberSn <= 0)  return null;
+        logger.debug(" === Query Member's memberSn: {} === ", memberSn);
+
+        Member member = this.memberService.findMemberBySn(memberSn);
+        if (ObjectUtils.isEmpty(member))    return null;
+        logger.debug(" === Query Member's memberId: {} === ", member.getMemberId());
+
+        return new MemberJoinResponseDto(member);
+    }
+
+    /**
+     * Get All Members Data
+     * GraphQL Schema Query : allMembers(count: Int, offset: Int): [Member]!
+     * @param count
+     * @param offset
+     * @return
+     */
+    public List<MemberJoinResponseDto> getAllMembers(final int count, final int offset) {
+        if (count <= 0 || offset <= 0)  return null;
+        logger.debug(" === Query Member List Count: {}, Offset: {} === ", count, offset);
+
+        return this.memberService.findAllMembers().stream()
+                .map(MemberJoinResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
