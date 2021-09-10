0f8077706e03e1ad9b4c1f79251ac71409194960
Jung Ian <jay2u8809@gmail.com>
2021-09-11 01:21:28 +0900

    Fix. add SpringBoot GraphQL Mutation : Delete

--- java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/domain/members/MemberRepository.java
+++ java/src/main/java/com/jay2u8809/codesamples/individual/study/bootandaws/domain/members/MemberRepository.java
@@ -2,6 +2,7 @@ package com.jay2u8809.codesamples.individual.study.bootandaws.domain.members;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +11,9 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // https://goodgid.github.io/Spring-Data-JPA-Query_Part_2/
-    // TODO Not Working Query
-    @Query("SELECT m FROM #{#entityName} m WHERE m.memberId like %?1% ORDER BY m.memberSn asc")
+    @Query("SELECT m FROM #{#entityName} m WHERE m.memberId = ?1 ORDER BY m.memberSn asc")
     Member findMemberById(String memberId);
 
-    @Query ("SELECT m FROM #{#entityName} m WHERE m.memberId like %?1% and m.memberSn > 0 ORDER BY m.memberSn DESC")
-    List<Member> findMembersById(String memberId);
+    @Query ("SELECT m FROM #{#entityName} m WHERE m.memberId like %:memId% AND m.memberSn > 0 ORDER BY m.memberSn DESC")
+    List<Member> findMembersById(@Param(("memId")) String memberId);
 }
