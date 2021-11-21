4093f45206fa9bdf7e805164062cae80a5f17c96
Jung Ian <jay2u8809@gmail.com>
2021-11-22 00:49:31 +0900

    Feat. convert json to csv serivce

--- javascript/nodejs/nest-sls-sample-project/src/api/api.module.ts
+++ javascript/nodejs/nest-sls-sample-project/src/api/api.module.ts
@@ -4,13 +4,17 @@ import { QrcodeController } from './qrcode/qrcode.controller';
 import { QrcodeService } from './qrcode/qrcode.service';
 import { TesseractService } from './ocr/tesseract/tesseract.service';
 import { TestController } from './test/test.controller';
+import { FileController } from './file/file.controller';
+import { CsvService } from './file/csv/csv.service';
 import { TestService } from './test/test.service';
 
 @Module({
   imports: [],
   controllers: [QrcodeController, OcrController],
   providers: [QrcodeService, TesseractService],
+    FileController,
     TestController,
+    CsvService, 
     TestService
 })
 export class ApiModule {}
---
+++ javascript/nodejs/nest-sls-sample-project/src/api/file/csv/csv.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import fs from 'fs';
+import { DataType } from '../../../common/code/data-type';
+
+const csvFilePath: string = __dirname + '/../temp/csv/';
+
+const TAG = 'CSV_SERVICE';
+
+@Injectable()
+export class CsvService {
+  async convertToCsv(data: any, type: DataType): Promise<any> {
+    let result: any;
+    switch (type) {
+      case DataType.JSON:
+        result = await this.convertJsonToCsv(data);
+        break;
+    }
+    // generate file
+    try {
+      const csvName: string =
+        `convert${type}ToCsv_` + new Date().getTime().toString() + '.csv';
+      fs.writeFileSync(csvFilePath + csvName, result.join('\n'));
+      console.log(TAG, `CSV FILE NAME: ${csvName}`);
+      return true;
+    } catch (e) {
+      console.error(`Fail to convert to csv`, e);
+      return false;
+    }
+  }
+
+  // === private ===
+  private convertJsonToCsv = (data: any): any => {
+    // title
+    const title: string[] = [];
+    Object.keys(data[0]).forEach((key) => {
+      title.push(`"${key.toUpperCase()}"`);
+    });
+    const headers: string = title.join(',');
+    // contents
+    const csvLines: string[] = [headers];
+    data.map((f) => {
+      const values: string[] = [];
+      Object.values(f).forEach((value) => {
+        values.push(`"${value}"`);
+      });
+      csvLines.push(values.join(','));
+    });
+    return csvLines;
+  };
+}
