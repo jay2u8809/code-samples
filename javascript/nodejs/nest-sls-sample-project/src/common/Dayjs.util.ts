f4d14d8aa9eaa3a6c2710fe0dd8051fd9d274870
Jung Ian <jay2u8809@gmail.com>
2022-02-07 00:35:49 +0900

    Feat. dayjs - add dayjs test code

---
+++ javascript/nodejs/nest-sls-sample-project/src/common/Dayjs.util.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import isoweek from 'dayjs/plugin/isoweek';
+import cumtomParseFormat from 'dayjs/plugin/customParseFormat';
+import { BadRequestException } from '@nestjs/common';
+
+dayjs.extend(utc);
+dayjs.extend(isoweek);
+dayjs.extend(cumtomParseFormat);
+
+export class DayjsUtil {
+  static koreaDate(): dayjs.Dayjs {
+    return this.localDate(9);
+  }
+
+  static localDate(utcOffset: number): dayjs.Dayjs {
+    return dayjs().utcOffset(utcOffset);
+  }
+
+  static koreaDateFromUnix(timeStamp: number): dayjs.Dayjs {
+    return dayjs.unix(timeStamp).utcOffset(9);
+  }
+
+  static parseLocalDate(date: string, utcOffset: number): dayjs.Dayjs {
+    const result = dayjs(date);
+    if (!result.isValid()) {
+      throw new BadRequestException();
+    }
+    return result.utcOffset(utcOffset);
+  }
+
+  static parseKoreaDate(date: string): dayjs.Dayjs {
+    return DayjsUtil.parseLocalDate(date, 9);
+  }
+
+  static toKoreaStampDate(date: dayjs.Dayjs): string {
+    return date.format('YYYY-MM-DD');
+  }
+}
--- javascript/nodejs/nest-sls-sample-project/src/etc/test/dayjs.test.spec.ts
+++ javascript/nodejs/nest-sls-sample-project/src/etc/test/dayjs.test.spec.ts
@@ -1,21 +1,59 @@
 import dayjs from 'dayjs';
+import { DayjsUtil } from '../../common/Dayjs.util';
 
-// npm i dayjs
-test('dayjs test',  () => {
+const TAG = 'DAYJS_TEST';
 
-  console.log(`Today(Date) : ${new Date().toISOString()}`); // 2001-01-01T01:06:33.018Z
+/**
+ * command
+ * $> npm i dayjs
+ * refs: https://github.com/iamkun/dayjs/blob/dev/docs/ko/README-ko.md
+ * api docs kr: https://github.com/iamkun/dayjs/blob/dev/docs/ko/API-reference.md
+ */
 
-  // Current Time
-  const today = dayjs();
-  console.log(`Today(DayJs) : ${today.toISOString()}`); // 2021-01-01T01:06:33.046Z
+describe('DayJs Test', () => {
+  beforeEach(() => {
+    console.log(TAG, DayjsUtil.koreaDate().toISOString());
+  });
 
-  // Basic Format
-  console.log(`Basic Format : ${dayjs().format()}`);  // 2021-01-01T10:06:33+09:00
+  it('dayjs test', () => {
+    console.log(TAG, `Today(Date) : ${new Date().toISOString()}`);
+    // [===== RESULT =====] 2022-02-05T16:59:10.117Z
 
-  // Format
-  console.log(`Format, YYYY-MM-DD, hh:mm:ss ddd : ${dayjs().format('YYYY-MM-DD, hh:mm:ss ddd')}`);
-  console.log(`Format DateType : ${dayjs(new Date()).format()}`);
+    const today = dayjs();
+    console.log(TAG, `Today(DayJs) : ${today.toISOString()}`);
+    // [===== RESULT =====] 2022-02-05T16:59:10.119Z
+    console.log(TAG, `Basic Format : ${dayjs().format()}`);
+    // [===== RESULT =====] 2022-02-06T01:59:10+09:00
+    console.log(TAG, `Format : ${dayjs().format('YYYY-MM-DD, hh:mm:ss ddd')}`);
+    // [===== RESULT =====] 2022-02-06, 01:59:10 Sun
+    console.log(TAG, `Format DateType : ${dayjs(new Date()).format()}`);
+    // [===== RESULT =====] 2022-02-06T01:59:10+09:00
+    console.log(TAG, `-30 days : ${today.subtract(30, 'd').toISOString()}`);
+    // [===== RESULT =====] 2022-01-06T16:59:10.119Z
+    console.log(TAG, `-1 Month : ${today.subtract(1, 'M').toISOString()}`);
+    // [===== RESULT =====] 2022-01-05T16:59:10.119Z
+  });
 
-  console.log(`-30 days : ${today.subtract(30, 'd').toISOString()}`);
-  console.log(`-1 Month : ${today.subtract(1, 'M').toISOString()}`);
+  describe('Dayjs Util test', () => {
+    it('toKoreaAwsDate', async () => {
+      let stamp = '2022-01-31T15:00:00.000Z';
+      let result: dayjs.Dayjs = DayjsUtil.parseKoreaDate(stamp);
+      console.log(TAG, DayjsUtil.toKoreaStampDate(result));
+      // [===== RESULT =====] 2022-02-01
+      stamp = '2022-01-31T02:00:00.000Z';
+      result = DayjsUtil.parseKoreaDate(stamp);
+      console.log(TAG, DayjsUtil.toKoreaStampDate(result));
+      // [===== RESULT =====] 2022-01-31
+    });
+    it('start of test', async () => {
+      const stamp = '2022-01-26T15:00:00.000Z';
+      let result: dayjs.Dayjs =
+        DayjsUtil.parseKoreaDate(stamp).startOf('isoWeek');
+      console.log(TAG, DayjsUtil.toKoreaStampDate(result));
+      // [===== RESULT =====] 2022-01-24
+      result = DayjsUtil.parseKoreaDate(stamp).startOf('week');
+      console.log(TAG, DayjsUtil.toKoreaStampDate(result));
+      // [===== RESULT =====] 2022-01-23
+    });
+  });
 });
