d423b2a51b9973dd787df1ac1ae3c9189a23e515
Jung Ian <jay2u8809@gmail.com>
2022-03-20 00:10:50 +0900

    Feat. How to use JUnit: test order

---
+++ java/src/test/java/com/jay2u8809/codesamples/individual/study/junit/JUnitUserTestOrder.java
@@ -0,0 +1,46 @@
+package com.jay2u8809.codesamples.individual.study.junit;
+
+import org.junit.jupiter.api.*;
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)     // 메소드 실행 순서와 상관없지만 각 테스트 메소드들을 연결하여 실행하고 싶다면 설정
+//@TestMethodOrder(MethodOrderer.MethodName.class)    // 테스트 메소드 실행 순서: 메소드명 순서
+//@TestMethodOrder(MethodOrderer.DisplayName.class)   // 테스트 메소드 실행 순서: @DisplayName(내용) 순서, 숫자 > 영대문자 > 영소문자
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)   // 테스트 메소드 실행 순서: @Order(번호) 순서, 1 > 2 > 3
+@TestMethodOrder(MethodOrderer.Random.class)    // 테스트 메소드 실행 순서: 랜덤
+public class JUnitUserTestOrder {
+
+//    @Order(5)
+//    @DisplayName("a")
+    @Test
+    void test_1() {
+        System.out.println("TEST 001");
+    }
+
+//    @Order(4)
+//    @DisplayName("4")
+    @Test
+    void test_2() {
+        System.out.println("TEST 002");
+    }
+
+//    @Order(3)
+//    @DisplayName("A")
+    @Test
+    void test_3() {
+        System.out.println("TEST 003");
+    }
+
+//    @Order(2)
+//    @DisplayName("3")
+    @Test
+    void test_4() {
+        System.out.println("TEST 004");
+    }
+
+//    @Order(1)
+//    @DisplayName("5")
+    @Test
+    void test_5() {
+        System.out.println("TEST 005");
+    }
+}
