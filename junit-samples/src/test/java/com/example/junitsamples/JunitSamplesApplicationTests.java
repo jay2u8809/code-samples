b5d831f6381ef4490aa89101a618aed0039d017a
Jung Ian <jay2u8809@gmail.com>
2022-03-21 01:42:17 +0900

    Feat. init Junit4 Project

---
+++ junit-samples/src/test/java/com/example/junitsamples/JunitSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitsamples;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
