dc015f8abd3ed0d210bb92ceafe2bc1cbe66b015
Jung Ian <jay2u8809@gmail.com>
2022-07-24 23:18:35 +0900

    Fix. csv-file

--- javascript/nodejs/nest-sls-sample-project/src/api/file/file.controller.ts
+++ javascript/nodejs/nest-sls-sample-project/src/api/file/file.controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CsvService } from './csv/csv.service';
 import { DataType } from '../../common/code/data-type';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('/api/v1/file')
+const TAG = 'FileController';
+
+@Controller('/v1/file')
 export class FileController {
   constructor(private readonly csvService: CsvService) {}
 
-  @Post('/json/convert/csv/')
+  @Post('/convert/json-to-csv/')
   public async convertJsonToCsv(@Body() data: any): Promise<any> {
+    console.assert(!!data, `${TAG}, json-is-empty`);
     return this.csvService.convertToCsv(data, DataType.JSON);
+  }
+
+  @Post('/upload/csv')
+  @UseInterceptors(FileInterceptor('file'))
+  public async uploadCsv(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<HttpStatus> {
+    console.log(TAG, 'upload-csv-file-name', file?.originalname);
+    const result = await this.csvService.saveFile(file);
+    return result ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
   }
 }
